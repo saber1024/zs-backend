21dcb769c11fc1ea93ffc04576c4cca146f32000
zhu <zhushuai19@gmail.com>
2020-11-04 22:11:45 +0800

    去重

--- controller/downloadController.js
+++ controller/downloadController.js
@@ -4,30 +4,28 @@ const getVideoInfo = handler(async (req, res) => {
   const url = decodeURIComponent(req.body.url);
 
   const info = await ytdl.getInfo(url);
-
-  const formats = info.formats.filter((x) => {
-    return x.container === "mp4";
-  });
-
-  var uniqueArr = [];
-
-  if (!formats.length === 0) {
-    uniqueArr = formats.reduce((acc, current) => {
-      const x = acc.find((item) => item.qualityLabel === current.qualityLabel);
-      if (!x) {
-        if (current.qualityLabel !== null) {
-          return acc.concat([current]);
-        }
-      } else {
-        return acc;
-      }
-    }, []);
-
-    res.json({
-      url: url,
-      format: uniqueArr,
-      code: 0,
+  if (info.formats.length > 0) {
+    const formats = info.formats.filter((x) => {
+      console.log(x.container);
+      return x.container === "mp4";
     });
+
+    if (formats.length > 0) {
+      const uniqueById = uniqByProp("qualityLabel");
+      const uniqueArr = uniqueById(formats);
+
+      res.json({
+        url: url,
+        format: uniqueArr,
+        code: 0,
+      });
+    } else {
+      res.json({
+        code: 0,
+        format: info.formats,
+        url: url,
+      });
+    }
   } else {
     res.json({
       code: 0,
@@ -36,5 +34,13 @@ const getVideoInfo = handler(async (req, res) => {
     });
   }
 });
+
+const uniqByProp = (prop) => (arr) =>
+  Object.values(
+    arr.reduce(
+      (acc, item) => (item && item[prop] && (acc[item[prop]] = item), acc), // using object mutation (faster)
+      {}
+    )
+  );
 
 module.exports = getVideoInfo;
